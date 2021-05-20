44f30bf5d8287bcca4b62e49e7452c4ba94b2d74
CyloTM <samuelchyke@hotmail.com>
2021-05-20 14:56:10 +0100

    LiveData Call Adapter for Retrofit

--- app/src/main/java/com/example/myapplication/api/ApiService.kt
+++ app/src/main/java/com/example/myapplication/api/ApiService.kt
@@ -1,18 +1,20 @@
 package com.example.myapplication.api
 
+import androidx.lifecycle.LiveData
 import com.example.myapplication.model.BlogPost
 import com.example.myapplication.model.User
+import com.example.myapplication.util.GenericApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("placeholder/blogs")
-    fun getBlogPosts(): List<BlogPost>
+    fun getBlogPosts(): LiveData<GenericApiResponse<List<BlogPost>>>
 
     @GET("placeholder/user/{userId}")
     fun getUser(
-        @Path("userId") userId: String):
-            User
+        @Path("userId") userId: String
+    ): LiveData<GenericApiResponse<User>>
 
 }
--- app/src/main/java/com/example/myapplication/api/MyRetrofitBuilder.kt
+++ app/src/main/java/com/example/myapplication/api/MyRetrofitBuilder.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.api
 
+import com.example.myapplication.util.LiveDataCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,6 +12,7 @@ object MyRetrofitBuilder {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(LiveDataCallAdapterFactory())
     }
 
     val apiService: ApiService by lazy {
